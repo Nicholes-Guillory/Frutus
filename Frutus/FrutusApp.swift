b3b951b5aa21668d5f5812e7dec5b2d304fd099b
Nicholes Guillory <nicholes.guillory@gmail.com>
2023-01-27 16:18:46 -0800

    Initial Commit

---
+++ Frutus/FrutusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrutusApp.swift
+//  Frutus
+//
+//  Created by Nicholes Guillory on 1/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FrutusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
